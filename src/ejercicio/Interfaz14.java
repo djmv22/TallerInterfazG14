ea5c1d080481ba45cc6971cbbb483261a63fa3df
Daniel Muñoz <dmunozcodisav@gmail.com>
2016-08-22 21:40:49 -0500

    Interfaz, funcionalidad y validacion creada

--- src/ejercicio/Interfaz14.java
+++ src/ejercicio/Interfaz14.java
@@ -5,6 +5,8 @@
  */
 package ejercicio;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author DanielDeJesus
@@ -27,21 +29,124 @@ public class Interfaz14 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtNDias = new javax.swing.JTextField();
+        txtVPagar = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        cmdCalcular = new javax.swing.JButton();
+        cmdBorrar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 14)); // NOI18N
+        jLabel1.setText("Ejercio Número 14: Hallar el valor por estadia en hotel 5 estrellas");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
+
+        jLabel2.setText("Nº de días:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 90, -1, -1));
+
+        txtNDias.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNDiasActionPerformed(evt);
+            }
+        });
+        txtNDias.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtNDiasKeyTyped(evt);
+            }
+        });
+        jPanel1.add(txtNDias, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 90, 60, -1));
+
+        txtVPagar.setEditable(false);
+        txtVPagar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtVPagarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtVPagar, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 140, 90, -1));
+
+        jLabel3.setText("Valor a pagar:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 140, -1, -1));
+
+        cmdCalcular.setText("Calcular");
+        cmdCalcular.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdCalcularActionPerformed(evt);
+            }
+        });
+        jPanel1.add(cmdCalcular, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 200, -1, -1));
+
+        cmdBorrar.setText("Borrar");
+        cmdBorrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdBorrarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(cmdBorrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 200, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 395, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtVPagarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtVPagarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtVPagarActionPerformed
+
+    private void cmdBorrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdBorrarActionPerformed
+        txtNDias.setText("");
+        txtVPagar.setText("");
+        
+        txtNDias.requestFocusInWindow();
+    }//GEN-LAST:event_cmdBorrarActionPerformed
+
+    private void txtNDiasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNDiasActionPerformed
+        
+    }//GEN-LAST:event_txtNDiasActionPerformed
+
+    private void txtNDiasKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtNDiasKeyTyped
+        char c=evt.getKeyChar();
+        
+        if(!Character.isDigit(c)){
+            getToolkit().beep();
+            evt.consume();
+        }
+    }//GEN-LAST:event_txtNDiasKeyTyped
+
+    private void cmdCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdCalcularActionPerformed
+        String res;
+        double ndias, op, opf;
+        
+        if(txtNDias.getText().trim().isEmpty()){
+            JOptionPane.showMessageDialog(this, "Digite el Nº de días", "Error", JOptionPane.ERROR_MESSAGE);
+            txtNDias.requestFocusInWindow();
+        }
+        else{
+            
+         ndias = Double.parseDouble(txtNDias.getText());
+         
+         op = (ndias * 200000) - 100000;
+         
+         res = String.valueOf(op);
+         txtVPagar.setText(res);
+            
+        } 
+        
+        
+    }//GEN-LAST:event_cmdCalcularActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +184,13 @@ public class Interfaz14 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdBorrar;
+    private javax.swing.JButton cmdCalcular;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtNDias;
+    private javax.swing.JTextField txtVPagar;
     // End of variables declaration//GEN-END:variables
 }
